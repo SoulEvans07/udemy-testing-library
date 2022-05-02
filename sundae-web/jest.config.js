46ed7eb065d601097973d20224a748ca9b30f85a
Adam Szi <szi.adam@simonyi.bme.hu>
2022-05-02 19:47:33 +0200

    cra empty project
    added "01-form-review"
    
    cra empty project

---
+++ sundae-web/jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+  setupFiles: ['react-app-polyfill/jsdom'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+  testEnvironment: 'jsdom',
+  testRunner: '<rootDir>/node_modules/jest-circus/runner.js',
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/config/jest/babelTransform.js',
+    // '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  modulePaths: [],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'json', 'scss', 'js', 'jsx', 'node', 'web.ts', 'web.tsx', 'web.js', 'web.jsx'],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  resetMocks: true,
+};
---
+++ sundae-web/src/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import './HomeScreen.scss';
+
+export function HomeScreen() {
+  return (
+    <div className="home-screen">
+      <header>
+        <a href="/" className="title">
+          ToDo App
+        </a>
+      </header>
+    </div>
+  );
+}
