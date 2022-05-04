485f92ac4fd6d09c7686541d57060398159ce6e6
Adam Szi <szi.adam@simonyi.bme.hu>
2022-05-04 22:50:38 +0200

    03-toppings-subtotal

--- sundae-web/src/pages/entry/test/TotalUpdates.test.tsx
+++ sundae-web/src/pages/entry/test/TotalUpdates.test.tsx
@@ -4,26 +4,44 @@ import { pricePerItem } from '../../../constants';
 import { mockScoopOptions } from '../../../mocks/getScoops';
 import { formatCurrency } from '../../../utils/currency-utils';
 import Options from '../Options';
+import { mockToppingOptions } from '../../../mocks/getToppings';
+import { Random } from '../../../utils/Random';
 
 describe('TotalUpdates', () => {
   test('update scoop subtotal when scoops change', async () => {
     render(<Options optionType="scoops" />);
 
+    let totalCost = 0;
     const scoopsSubTotal = screen.getByText('Scoops total: $', { exact: false });
-    expect(scoopsSubTotal).toHaveTextContent('$0.00');
+    expect(scoopsSubTotal).toHaveTextContent(formatCurrency(totalCost));
 
-    const firstScoopCount = 1;
-    const firstScoopCost = firstScoopCount * pricePerItem.scoops;
-    const firstScoop = await screen.findByRole('spinbutton', { name: mockScoopOptions[0].name });
-    userEvent.clear(firstScoop);
-    userEvent.type(firstScoop, String(firstScoopCount));
-    expect(scoopsSubTotal).toHaveTextContent(formatCurrency(firstScoopCost));
+    for (let i = 0; i < mockScoopOptions.length; i++) {
+      const scoop = mockScoopOptions[i];
+      const selectCount = i + 1;
+      totalCost += selectCount * pricePerItem.scoops;
+      const scoopInput = await screen.findByRole('spinbutton', { name: scoop.name });
+      userEvent.clear(scoopInput);
+      userEvent.type(scoopInput, String(selectCount));
+      expect(scoopsSubTotal).toHaveTextContent(formatCurrency(totalCost));
+    }
+  });
 
-    const secondScoopCount = 2;
-    const secondScoopCost = secondScoopCount * pricePerItem.scoops;
-    const secondScoop = await screen.findByRole('spinbutton', { name: mockScoopOptions[1].name });
-    userEvent.clear(secondScoop);
-    userEvent.type(secondScoop, String(secondScoopCount));
-    expect(scoopsSubTotal).toHaveTextContent(formatCurrency(firstScoopCost + secondScoopCost));
+  test('update toppings subtotal when toppings change', async () => {
+    render(<Options optionType="toppings" />);
+
+    const toppingsSubTotal = screen.getByText('Toppings total: $', { exact: false });
+    expect(toppingsSubTotal).toHaveTextContent('$0.00');
+
+    for (let i = 0; i < mockToppingOptions.length; i++) {
+      const topping = mockToppingOptions[i];
+      const toppingCheckbox = await screen.findByRole('checkbox', { name: topping.name });
+      userEvent.click(toppingCheckbox);
+      expect(toppingsSubTotal).toHaveTextContent(formatCurrency((i + 1) * pricePerItem.toppings));
+    }
+
+    const topping = Random.choose(mockToppingOptions);
+    const toppingCheckbox = await screen.findByRole('checkbox', { name: topping.name });
+    userEvent.click(toppingCheckbox);
+    expect(toppingsSubTotal).toHaveTextContent(formatCurrency((mockToppingOptions.length - 1) * pricePerItem.toppings));
   });
 });
---
+++ sundae-web/src/utils/Random.ts
@@ -0,0 +1,21 @@
+export class Random {
+  static number(size = 1): number {
+    return Math.floor(Math.random() * size);
+  }
+
+  static string(length: number): string {
+    if (length < 1) throw new InvalidArgumentException('length must be bigger than 0');
+    return new Array(length)
+      .fill(null)
+      .map(() => String.fromCharCode(97 + this.number(26)))
+      .join('');
+  }
+
+  static choose<T>(array: T[]): T {
+    return array[this.number(array.length - 1)];
+  }
+}
+
+export class InvalidArgumentException extends Error {
+  name = 'InvalidArgumentException';
+}
